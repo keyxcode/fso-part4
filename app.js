bf455eeeecdba6129962b2cb9cd0b12b3b9d3bd6
keyxcode <keyxcode@gmail.com>
2023-04-08 17:47:14 +0700

    Separate the config util

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config();
+const config = require("./utils/config");
 
 const app = express();
 
@@ -14,7 +14,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema);
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(config.MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
