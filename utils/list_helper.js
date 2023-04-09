2071d9ca3830367041474ca256d339bd2aa7d607
keyxcode <keyxcode@gmail.com>
2023-04-09 23:59:44 +0700

    Define favoriteBlog function

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,7 +4,13 @@ const dummy = (blogs) => 1;
 const totalLikes = (blogs) =>
   blogs.reduce((total, current) => total + current.likes, 0);
 
+const favoriteBlog = (blogs) =>
+  blogs.reduce((prev, current) =>
+    prev.likes > current.likes ? prev : current
+  );
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
