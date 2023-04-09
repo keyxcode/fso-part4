9dfe6551d00f8ac6bcb71947b7a6a81059a78fd9
keyxcode <keyxcode@gmail.com>
2023-04-09 23:52:24 +0700

    Solution 4.4: implement and test totalLikes function

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,6 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => 1;
 
+const totalLikes = (blogs) =>
+  blogs.reduce((total, current) => total + current.likes, 0);
+
 module.exports = {
   dummy,
+  totalLikes,
 };
