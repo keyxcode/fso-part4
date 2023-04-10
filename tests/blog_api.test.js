96c2cca8f2d21387c7529ef181192a9c28399fdb
keyxcode <keyxcode@gmail.com>
2023-04-10 19:54:43 +0700

    Refactor api test into describe blocks

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -8,99 +8,99 @@ const api = supertest(app);
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-
-  const blogObjects = helper.initialBlogs.map((b) => new Blog(b));
-  const promiseArray = blogObjects.map((b) => b.save());
-
-  await Promise.all(promiseArray);
+  await Blog.insertMany(helper.initialBlogs);
 }, 100000);
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("when there are initially some blogs saved", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs");
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
+
+  test("blogs are returned with id prop", async () => {
+    const response = await api.get("/api/blogs");
+
+    response.body.forEach((blog) => expect(blog.id).toBeDefined());
+  });
 });
 
-test("all blogs are returned", async () => {
-  const response = await api.get("/api/blogs");
+describe("addition of a new blog", () => {
+  test("a valid blog can be added", async () => {
+    const newBlog = {
+      title: "How to play jazz",
+      author: "Cadence Phan",
+      url: "https://google.com/",
+      likes: 3,
+    };
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
-});
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-test("blogs are returned with id prop", async () => {
-  const response = await api.get("/api/blogs");
+    const blogsAtTheEnd = await helper.blogsInDb();
+    expect(blogsAtTheEnd).toHaveLength(helper.initialBlogs.length + 1);
 
-  response.body.forEach((blog) => expect(blog.id).toBeDefined());
-});
+    const contents = blogsAtTheEnd.map((b) => b.title);
+    expect(contents).toContain("How to play jazz");
+  });
 
-test("a valid blog can be added", async () => {
-  const newBlog = {
-    title: "How to play jazz",
-    author: "Cadence Phan",
-    url: "https://google.com/",
-    likes: 3,
-  };
+  test("likes default to 0 if missing", async () => {
+    const newBlog = {
+      title: "How to play jazz",
+      author: "Cadence Phan",
+      url: "https://google.com/",
+    };
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-  const blogsAtTheEnd = await helper.blogsInDb();
-  expect(blogsAtTheEnd).toHaveLength(helper.initialBlogs.length + 1);
+    const blogsAtTheEnd = await helper.blogsInDb();
+    expect(blogsAtTheEnd).toHaveLength(helper.initialBlogs.length + 1);
 
-  const contents = blogsAtTheEnd.map((b) => b.title);
-  expect(contents).toContain("How to play jazz");
-});
+    const newBlogFromDb = blogsAtTheEnd.find(
+      (b) => b.title === "How to play jazz"
+    );
+    expect(newBlogFromDb.likes).toBe(0);
+  });
 
-test("likes default to 0 if missing", async () => {
-  const newBlog = {
-    title: "How to play jazz",
-    author: "Cadence Phan",
-    url: "https://google.com/",
-  };
+  test("400 if title is missing", async () => {
+    const newBlog = {
+      author: "Cadence Phan",
+      url: "https://google.com/",
+      likes: 3,
+    };
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    await api.post("/api/blogs").send(newBlog).expect(400);
 
-  const blogsAtTheEnd = await helper.blogsInDb();
-  expect(blogsAtTheEnd).toHaveLength(helper.initialBlogs.length + 1);
+    const blogsAtTheEnd = await helper.blogsInDb();
+    expect(blogsAtTheEnd).toHaveLength(helper.initialBlogs.length);
+  });
 
-  const newBlogFromDb = blogsAtTheEnd.find(
-    (b) => b.title === "How to play jazz"
-  );
-  expect(newBlogFromDb.likes).toBe(0);
-});
+  test("400 if  url is missing", async () => {
+    const newBlog = {
+      title: "How to play jazz",
+      author: "Cadence Phan",
+      likes: 3,
+    };
 
-test("400 if title is missing", async () => {
-  const newBlog = {
-    author: "Cadence Phan",
-    url: "https://google.com/",
-    likes: 3,
-  };
+    await api.post("/api/blogs").send(newBlog).expect(400);
 
-  await api.post("/api/blogs").send(newBlog).expect(400);
-
-  const blogsAtTheEnd = await helper.blogsInDb();
-  expect(blogsAtTheEnd).toHaveLength(helper.initialBlogs.length);
-});
-
-test("400 if  url is missing", async () => {
-  const newBlog = {
-    title: "How to play jazz",
-    author: "Cadence Phan",
-    likes: 3,
-  };
-
-  await api.post("/api/blogs").send(newBlog).expect(400);
-
-  const blogsAtTheEnd = await helper.blogsInDb();
-  expect(blogsAtTheEnd).toHaveLength(helper.initialBlogs.length);
+    const blogsAtTheEnd = await helper.blogsInDb();
+    expect(blogsAtTheEnd).toHaveLength(helper.initialBlogs.length);
+  });
 });
 
 afterAll(async () => {
