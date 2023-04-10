ac6c005a5751b3cfb5514fb563d99dce1df28c18
keyxcode <keyxcode@gmail.com>
2023-04-10 17:40:11 +0700

    Solution 4.8: refactor get all route with async await

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
+require("express-async-errors");
 
 const app = express();
 mongoose.connect(config.MONGODB_URI);
