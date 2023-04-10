113cbe69d9401e0531fef2fd9b5e1e358a17ced6
keyxcode <keyxcode@gmail.com>
2023-04-10 17:30:22 +0700

    Create test helper and define test for get all

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,26 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
+const helper = require("./test_helper");
+const Blog = require("../models/blog");
 
 const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  const blogObjects = helper.initialBlogs.map((b) => new Blog(b));
+  const promiseArray = blogObjects.map((b) => b.save());
+
+  await Promise.all(promiseArray);
+}, 100000);
+
+test("blogs are returned as json and with the right number of posts", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
 
 afterAll(async () => {
   await mongoose.connection.close();
