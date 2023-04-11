a0551558844d75f27ff5fdc28469f33aece84082
keyxcode <keyxcode@gmail.com>
2023-04-12 00:32:27 +0700

    Add logger when connecting to db

--- app.js
+++ app.js
@@ -6,11 +6,21 @@ const config = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
+const logger = require("./utils/logger");
 const middleware = require("./utils/middleware");
 
 const app = express();
+
 mongoose.set("strictQuery", false);
-mongoose.connect(config.MONGODB_URI);
+logger.info("connecting to", config.MONGODB_URI);
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
 
 app.use(cors());
 app.use(express.json());
@@ -20,7 +30,7 @@ app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
 
-app.use(middleware.errorHandler);
 app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
