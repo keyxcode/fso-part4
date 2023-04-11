751661947de260f9b985a3a30f98afbe2920de21
keyxcode <keyxcode@gmail.com>
2023-04-11 10:11:04 +0700

    Solution 4.15: implement user post and get route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 require("express-async-errors");
 
 const app = express();
@@ -11,5 +12,6 @@ mongoose.connect(config.MONGODB_URI);
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
